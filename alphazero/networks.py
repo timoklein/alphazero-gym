60c6c46540659d8ad3eb706b862a34e3ef2d2dfc
Timo K <tkleia34@gmail.com>
2020-08-27 08:07:15 +0200

    add continuous network sampling

--- alphazero/networks.py
+++ alphazero/networks.py
@@ -3,10 +3,11 @@ import numpy as np
 import torch
 from torch import nn
 import torch.nn.functional as F
+from torch.distributions import Normal
 from abc import ABC, abstractmethod
 
-class Network(ABC, nn.Module):
 
+class Network(ABC, nn.Module):
     @abstractmethod
     def predict_V(self):
         ...
@@ -14,6 +15,7 @@ class Network(ABC, nn.Module):
     @abstractmethod
     def predict_pi(self):
         ...
+
 
 class NetworkDiscrete(Network):
     def __init__(
@@ -68,12 +70,21 @@ class NetworkDiscrete(Network):
 
 class NetworkContinuous(Network):
     def __init__(
-        self, state_dim: int, action_dim: int, n_hidden_layers: int, n_hidden_units: int
+        self,
+        state_dim: int,
+        action_dim: int,
+        n_hidden_layers: int,
+        n_hidden_units: int,
+        log_max: int = 2,
+        log_min: int = -20,
     ) -> None:
         super().__init__()
 
         self.state_dim = state_dim
         self.action_dim = action_dim
+
+        self.LOG_SIG_MAX = log_max
+        self.LOG_SIG_MIN = log_min
 
         self.n_hidden_layers = n_hidden_layers
         self.n_hidden_units = n_hidden_units
@@ -93,10 +104,9 @@ class NetworkContinuous(Network):
     def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         x = F.elu(self.in_layer(x))
         x = self.hidden(x)
-        # no need for softmax, can be computed directly from cross-entropy loss
-        pi_hat = self.policy_head(x)
+        mean, log_std = self.policy_head(x)
         V_hat = self.value_head(x)
-        return pi_hat, V_hat
+        return mean, log_std, V_hat
 
     @torch.no_grad()
     def predict_V(self, x: torch.Tensor) -> np.array:
@@ -107,12 +117,12 @@ class NetworkContinuous(Network):
         self.train()
         return V_hat.detach().cpu().numpy()
 
-    # TODO: Implement
-    @torch.no_grad()
-    def predict_pi(self, x: torch.Tensor) -> np.array:
-        self.eval()
+    def sample(self, x: torch.Tensor) -> Tuple[np.array, np.array, torch.Tensor]:
         x = F.elu(self.in_layer(x))
-        x = self.hidden(x)
-        pi_hat = F.softmax(self.policy_head(x), dim=-1)
-        self.train()
-        return pi_hat.detach().cpu().numpy()
+        mean, log_std = self.policy_head(self.hidden(x))
+        log_std = torch.clamp(log_std, min=self.LOG_SIG_MIN, max=self.LOG_SIG_MAX)
+        std = log_std.exp()
+        normal = Normal(mean, std)
+        action = normal.sample()
+        log_prob = normal.log_prob(action)
+        return action.detach().cpu().numpy(), mean.detach().cpu().numpy(), log_prob
