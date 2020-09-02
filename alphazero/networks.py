2452825743dfa649c02a04a2aeeb6c5eb923552c
Timo K <tkleia34@gmail.com>
2020-09-02 09:21:25 +0200

    implement sac tricks

--- alphazero/networks.py
+++ alphazero/networks.py
@@ -68,7 +68,7 @@ class NetworkContinuous(nn.Module):
         n_hidden_layers: int,
         n_hidden_units: int,
         log_max: int = 2,
-        log_min: int = -20,
+        log_min: int = -5,
     ) -> None:
         super().__init__()
 
@@ -80,8 +80,8 @@ class NetworkContinuous(nn.Module):
         self.action_bias = torch.FloatTensor(
             (action_space_high + action_space_low) / 2.)
 
-        self.LOG_SIG_MAX = log_max
-        self.LOG_SIG_MIN = log_min
+        self.LOG_STD_MAX = log_max
+        self.LOG_STD_MIN = log_min
 
         self.n_hidden_layers = n_hidden_layers
         self.n_hidden_units = n_hidden_units
@@ -104,6 +104,10 @@ class NetworkContinuous(nn.Module):
         x = self.hidden(x)
         mean = self.mean_head(x)
         log_std = self.std_head(x)
+        log_std = torch.tanh(log_std)
+        # Trick from OpenAI spinning up to scale log standard deviation
+        log_std = self.LOG_STD_MIN + 0.5 * (self.LOG_STD_MAX - self.LOG_STD_MIN) * (log_std + 1)
+
         V_hat = self.value_head(x)
         return mean, log_std, V_hat
 
@@ -116,14 +120,18 @@ class NetworkContinuous(nn.Module):
         self.train()
         return V_hat.detach().cpu().numpy()
 
-    def sample(self, x: torch.Tensor) -> Tuple[np.array, np.array, torch.Tensor]:
-        x = F.elu(self.in_layer(x))
-        x = self.hidden(x)
-        mean = self.mean_head(x)
-        log_std = self.std_head(x)
-        log_std = torch.clamp(log_std, min=self.LOG_SIG_MIN, max=self.LOG_SIG_MAX)
+    def sample(self, x: torch.Tensor) -> Tuple[np.array, np.array, torch.Tensor, torch.Tensor]:
+        mean, log_std, V_hat = self.forward(x)
         std = log_std.exp()
         normal = Normal(mean, std)
-        action = normal.sample()
-        log_prob = normal.log_prob(action)
-        return action.detach().cpu().numpy(), log_prob, mean.detach().cpu().numpy()
+        # no need reparameterization trick (mean + std * N(0,1))
+        x_t = normal.sample()
+        y_t = torch.tanh(x_t)
+        action = y_t * self.action_scale + self.action_bias
+        log_prob = normal.log_prob(x_t)
+        # Enforcing Action Bound
+        # This is the correction for tanh squashing of the log_std and actions
+        log_prob -= torch.log(self.action_scale * (1 - y_t.pow(2)) +  1e-6)
+        log_prob = log_prob.sum(1, keepdim=True)
+        mean = torch.tanh(mean) * self.action_scale + self.action_bias
+        return action.detach().cpu().numpy(), mean.detach().cpu().numpy(), log_prob, V_hat
